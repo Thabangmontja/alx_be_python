4c402a98054b0197fc7d378a4e08067da4e3dd61
Thabang Montja <thabangmontja20@gmail.com>
2024-09-14 20:54:48 +0200

    Add simple interest calculation script

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+#Create Variables
+principal = 1000
+rate = 0.05 #this is the 5% annual interest rate.
+time = 3 #3 years
+
+#Calculation of simple interest
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
